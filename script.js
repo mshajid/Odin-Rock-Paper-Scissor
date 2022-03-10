69c3ffbd8de1b3e0e0905a8d6af1866e17e7a32a
Shajid <shajid1biz@gmail.com>
2022-03-11 01:58:32 +0530

    Added Computer Play Function

---
+++ script.js
@@ -0,0 +1,13 @@
+function computerPlay() {
+    const choices = Math.floor(Math.random()* 3);
+    if (choices == 0) {
+        console.log("Rock");
+    } else if (choices == 1) {
+        console.log("Paper");
+    } else if (choices == 2) {
+        console.log("Scissor");
+    }
+    return choices
+}
+
+computerPlay();
