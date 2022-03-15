6305af23bff5ea52eb6581ce8fbb4fd29fc8ba42
shajid <shajid1biz@gmail.com>
2022-03-16 00:49:08 +0530

    Final Phase Game Function

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+let winner = 0;
+let humanScore = 0;
+let computerScore = 0;
+
 function computerPlay() {
     const choices = Math.floor(Math.random() * 3); 
     if (choices == 0) {
@@ -13,32 +17,55 @@ function computerPlay() {
    return choices
 }
 
+game();
+
+function game () {
+    while (humanScore <= 5 && computerScore <= 5) {
+        let playerSelection = prompt("Enter Rock, Paper, Scissor");
+        let computerSelection = computerPlay();
+        // playerSelection = playerSelection.toUpperCase();
+        // computerSelection = computerSelection.toUpperCase();
+        alert(playRound(playerSelection, computerSelection));
+    }
+    alert(winCondition());
+}
+
+
 
 function playRound (playerSelection, computerSelection) {
     
     if ((playerSelection == "Rock" && computerSelection == "Scissor") || 
         (playerSelection == "Paper" && computerSelection == "Rock" ) ||
         (playerSelection == "Scissor" && computerSelection == "Paper")) {
+            humanScore += 1;
             return "Player Wins " + playerSelection + " Beats " + computerSelection;
 
     } else if ((computerSelection == "Rock" && playerSelection == "Scissor") ||
         (computerSelection == "Paper" && playerSelection == "Rock") ||
         (computerSelection == "Scissor" && playerSelection == "Paper")) {
+            computerScore += 1;
             return "Computer Wins " + computerSelection + " Beats " + playerSelection;
 
     } else if ((playerSelection == "Rock" && computerSelection =="Rock") ||
         (playerSelection == "Paper" && computerSelection == "Paper") ||
         (playerSelection == "Scissor" && computerSelection == "Scissor")) {
             return "It's a Tie " + playerSelection + " Tied " + computerSelection;
+    } else return "Enter a Valid Move.";
+}
+
+function winCondition () {
+    if (humanScore == 5) {
+        return "Player Wins";
+    } else if (computerScore == 5) {
+        return "Computer Wins";
     }
 }
 
-
-let computer = computerPlay();
-const playerSelection = "Rock";
-const computerSelection = computer;
-console.log(computer);
-console.log(playRound(playerSelection, computerSelection));
+// let computer = computerPlay();
+// const playerSelection = "Rock";
+// const computerSelection = computer;
+// console.log(computer);
+// console.log(playRound(playerSelection, computerSelection));
 
 
 
